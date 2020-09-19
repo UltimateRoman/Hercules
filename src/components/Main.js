7ba378631af9a7b5de2e04206f167d61b13432cd
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-09-19 15:32:23 +0530

    Bolt features added

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,6 +9,16 @@ class Main extends Component {
           device_id: '',
           api_key: ''
         };
+        this.switchOn = this.switchOn.bind(this);
+        this.switchOff = this.switchOff.bind(this);
+    }
+
+    switchOn(event) {
+        fetch(`https://cloud.boltiot.com/remote/${this.state.api_key}/digitalWrite?pin=${event.target.name}&state=HIGH&deviceName=${this.state.device_id}`)
+    }
+
+    switchOff(event) {
+        fetch(`https://cloud.boltiot.com/remote/${this.state.api_key}/digitalWrite?pin=${event.target.name}&state=LOW&deviceName=${this.state.device_id}`)
     }
 
     render() {
@@ -22,7 +32,9 @@ class Main extends Component {
                     <h1 style={{color: "royalblue"}}>Home-Automation</h1>
                     <h2>Enter your Bolt IoT device Credentials</h2>
                     <Form onSubmit={async(event) => {
-
+                        event.preventDefault()
+                        this.setState({ device_id: this.devid.value })
+                        this.setState({ api_key: this.apik.value })
                     }}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Control 
@@ -47,21 +59,21 @@ class Main extends Component {
                     </Form>
                     <p>&nbsp;&nbsp;</p>
                     <div class="coupon">
+                        <h3>Switch 0</h3>
+                        <Button variant="success" name="0" onClick={this.switchOn}>On</Button>{' '}
+                        <Button variant="danger" name="0" onClick={this.switchOff}>Off</Button>
+                    </div>
+                    <p>&nbsp;&nbsp;</p>
+                    <div class="coupon">
                         <h3>Switch 1</h3>
-                        <Button variant="success">On</Button>{' '}
-                        <Button variant="danger">Off</Button>
+                        <Button variant="success" name="1" onClick={this.switchOn}>On</Button>{' '}
+                        <Button variant="danger" name="1" onClick={this.switchOff}>Off</Button>
                     </div>
                     <p>&nbsp;&nbsp;</p>
                     <div class="coupon">
                         <h3>Switch 2</h3>
-                        <Button variant="success">On</Button>{' '}
-                        <Button variant="danger">Off</Button>
-                    </div>
-                    <p>&nbsp;&nbsp;</p>
-                    <div class="coupon">
-                        <h3>Switch 3</h3>
-                        <Button variant="success">On</Button>{' '}
-                        <Button variant="danger">Off</Button>
+                        <Button variant="success" name="2" onClick={this.switchOn}>On</Button>{' '}
+                        <Button variant="danger" name="2" onClick={this.switchOff}>Off</Button>
                     </div>
                     <p>&nbsp;&nbsp;</p>                    
                     </center>
